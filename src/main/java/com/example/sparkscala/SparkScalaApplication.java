57b0b4a7b42b3e019aee57c7ba8954e193958d4c
qxb <qq@mail.com>
2019-04-03 11:37:25 +0800

    333

---
+++ src/main/java/com/example/sparkscala/SparkScalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparkscala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkScalaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkScalaApplication.class, args);
+    }
+
+}
